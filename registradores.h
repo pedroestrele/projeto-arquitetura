cde921e2135bf52c1157dda0a2d32b2be5ed32b4
Arthur Correia <156236396+arthurscorreia@users.noreply.github.com>
2025-02-09 10:40:58 -0300

    adição dos registradores flags
    OBS - trabalhei com signed ints, então somente utilizei os registradores flag ZF (zero flag), SF (sign flag), OF (overflow flag)

--- registradores.h
+++ registradores.h
@@ -5,102 +5,123 @@ using namespace std;
 
 ///////////GERAIS///////////
 class RegGerais{
-    public:
-        int EAX;
-        int EBX;
-        int ECX;
-        int EDX;
-    RegGerais() : EAX(0), EBX(0), ECX(0), EDX(0) {}
-    
-    RegGerais(int EAX, int EBX, int ECX, int EDX){
-      this->EAX = EAX;
-      this->EBX = EBX;
-      this->ECX = ECX;
-      this->EDX = EDX;
-    }
+	public:
+        	int EAX;
+        	int EBX;
+        	int ECX;
+        	int EDX;
+   	RegGerais() : EAX(0), EBX(0), ECX(0), EDX(0) {}
 
-    void mostrar_dados(){
-        cout<<"Registradores Gerais: "<<endl;
-        cout<<"EAX: "<<EAX<<endl;
-        cout<<"EBX: "<<EBX<<endl;
-        cout<<"ECX: "<<ECX<<endl;
-        cout<<"EDX: "<<EDX<<endl;
-    }
+    	RegGerais(int EAX, int EBX, int ECX, int EDX){
+      		this->EAX = EAX;
+      		this->EBX = EBX;
+      		this->ECX = ECX;
+      		this->EDX = EDX;
+		}
+
+    	void mostrar_dados(){
+        	cout<<"Registradores Gerais: "<<endl;
+        	cout<<"EAX: "<<EAX<<endl;
+        	cout<<"EBX: "<<EBX<<endl;
+        	cout<<"ECX: "<<ECX<<endl;
+        	cout<<"EDX: "<<EDX<<endl;
+    	}
 
 };
 
 ///////////SELETORES DE SEGMENTO///////////
 class RegSeletorSegmentos{
-  	public:
-      Endereco<16> CS;
-      Endereco<16> SS;
-      Endereco<16> DS;
+    	public:
+      		Endereco<16> CS;
+      		Endereco<16> SS;
+      		Endereco<16> DS;
 
-    RegSeletorSegmentos() : CS(Endereco<16>()), SS(Endereco<16>()), DS(Endereco<16>()){}
-    RegSeletorSegmentos(string CS, string SS, string DS){
-      this->CS.end_hex = CS;
-      this->SS.end_hex = SS;
-      this->DS.end_hex = DS;
-    }
+    	RegSeletorSegmentos() : CS(Endereco<16>()), SS(Endereco<16>()), DS(Endereco<16>()){}
+    	RegSeletorSegmentos(string CS, string SS, string DS){
+      		this->CS.end_hex = CS;
+      		this->SS.end_hex = SS;
+      		this->DS.end_hex = DS;
+    	}
 
 
-    void obter_entrada(){
-        cout<<"Digite os seletores de cada segmento:";
+    	void obter_entrada(){
+        	cout<<"Digite os seletores de cada segmento:";
 
-        cout<<"\nCS: ";
-        cin>>this->CS.end_hex;
+        	cout<<"\nCS: ";
+        	cin>>this->CS.end_hex;
 
-        cout<<"SS: ";
-        cin>>this->SS.end_hex;
+       		cout<<"SS: ";
+        	cin>>this->SS.end_hex;
 
-        cout<<"DS: ";
-        cin>>this->DS.end_hex;
-    }
+        	cout<<"DS: ";
+        	cin>>this->DS.end_hex;
+    	}
 
-    void mostrar_dados(){
-      cout  << "CS: " << this->CS.end_hex << endl;
-      cout  << "SS: " << this->SS.end_hex << endl;
-      cout  << "DS: " << this->DS.end_hex << endl;
-    }
+    	void mostrar_dados(){
+      		cout  << "CS: " << this->CS.end_hex << endl;
+     	 	cout  << "SS: " << this->SS.end_hex << endl;
+      		cout  << "DS: " << this->DS.end_hex << endl;
+    	}
 };
 
 ///////////OFFSET///////////
 class RegOffsets{
-  	public:
-		Endereco<32> EIP;
-		Endereco<32> EBP;
-		Endereco<32> ESP;
-		Endereco<32> ESI;
-		Endereco<32> EDI;
+    	public:
+    		Endereco<32> EIP;
+    		Endereco<32> EBP;
+    		Endereco<32> ESP;
+    		Endereco<32> ESI;
+   		Endereco<32> EDI;
 
-    RegOffsets() : EIP(Endereco<32>()), EBP(Endereco<32>()), ESP(Endereco<32>()), ESI(Endereco<32>()), EDI(Endereco<32>()){}
-	RegOffsets(string EIP, string EBP, string ESP, string ESI, string EDI){
-		this->EIP.end_hex = EIP;
-		this->EBP.end_hex = EBP;
-		this->ESP.end_hex = ESP;
-		this->ESI.end_hex = ESI;
-		this->EDI.end_hex = EDI;
-	}
+    	RegOffsets() : EIP(Endereco<32>()), EBP(Endereco<32>()), ESP(Endereco<32>()), ESI(Endereco<32>()), EDI(Endereco<32>()){}
+    	RegOffsets(string EIP, string EBP, string ESP, string ESI, string EDI){
+    		this->EIP.end_hex = EIP;
+    		this->EBP.end_hex = EBP;
+    		this->ESP.end_hex = ESP;
+    		this->ESI.end_hex = ESI;
+    		this->EDI.end_hex = EDI;
+  	}
 
-    void obter_entrada(){
-        string eip_value, ebp_value;
-        cout<<"Digite o endereço inicial do segmento EIP: ";
-        cin>>eip_value;
-        this->EIP = Endereco<32>(eip_value);
+    	void obter_entrada(){
+        	string eip_value, ebp_value;
+        	cout<<"Digite o endereço inicial do segmento EIP: ";
+        	cin>>eip_value;
+        	this->EIP = Endereco<32>(eip_value);
 
-        cout<<"Digite o endereço inicial do segmento EBP: ";
-        cin>>ebp_value;
-        this->EBP = Endereco<32>(ebp_value);
-        this->ESP = Endereco<32>(ebp_value);
-    }
+        	cout<<"Digite o endereço inicial do segmento EBP: ";
+        	cin>>ebp_value;
+        	this->EBP = Endereco<32>(ebp_value);
+        	this->ESP = Endereco<32>(ebp_value);
+   	}
 
-    void mostrar_dados(){
-        cout<<endl<<"Offset: "<<endl;
-        cout  << "EIP: " << this->EIP.end_hex << endl;
-        cout  << "EBP: " << this->EBP.end_hex << endl;
-        cout  << "ESP: " << this->ESP.end_hex << endl;
-        cout  << "ESI: " << this->ESI.end_hex << endl;
-        cout  << "EDI: " << this->EDI.end_hex << endl;
-    }
-
+    	void mostrar_dados(){
+        	cout<<endl<<"Offset: "<<endl;
+        	cout  << "EIP: " << this->EIP.end_hex << endl;
+        	cout  << "EBP: " << this->EBP.end_hex << endl;
+        	cout  << "ESP: " << this->ESP.end_hex << endl;
+        	cout  << "ESI: " << this->ESI.end_hex << endl;
+        	cout  << "EDI: " << this->EDI.end_hex << endl;
+    	}
 };
+
+///////////FLAG///////////
+class RegFlag{
+    	public:
+    		Endereco<32> ZF;
+    		Endereco<32> SF;
+    		Endereco<32> OF;
+    	RegFlag() : ZF(Endereco<32>()), SF(Endereco<32>()), OF(Endereco<32>()){}
+    	RegFlag(string ZF, string SF, string OF){
+    		this->ZF.end_hex = ZF;
+    		this->SF.end_hex = SF;
+    		this->OF.end_hex = OF;
+   	}
+
+    	void mostrar_dados(){          
+          	cout << "Flags: " << endl;
+         	cout << "ZF: " << ZF.end_hex << endl;
+          	cout << "SF: " << SF.end_hex << endl;
+          	cout << "OF: " << OF.end_hex << endl;
+        }
+};
+
