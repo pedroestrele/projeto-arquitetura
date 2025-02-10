25ec4f4b4775c93982707b709028168da735fd3f
pedroestrele <pedromarcosnec@gmail.com>
2025-02-09 21:14:03 -0300

    renomeada função binaryToHex para longToHex para representar mais corretamente o que faz

--- hex.h
+++ hex.h
@@ -14,9 +14,9 @@ class HexNumber
 	HexNumber() : value ("0") {}
 	HexNumber (string value) : value (value) {}
 	
-	HexNumber(long value_in_binary, bool t) : value (binaryToHex(value_in_binary)) {}
+	HexNumber(long value_in_binary, bool t) : value (longToHex(value_in_binary)) {}
 
-	static string binaryToHex(long binary) {
+	static string longToHex(long binary) {
     	
 		stringstream ss;
     	ss << hex << binary;
